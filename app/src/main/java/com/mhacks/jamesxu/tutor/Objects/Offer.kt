134deaf6d0bf7688329bc6a38b7040fd002ddaff
jamesxu <jamesxu@umich.edu>
2018-10-13 23:20:44 -0400

    no constructor

--- app/src/main/java/com/mhacks/jamesxu/tutor/Objects/Offer.kt
+++ app/src/main/java/com/mhacks/jamesxu/tutor/Objects/Offer.kt
@@ -1,3 +1,5 @@
 package com.mhacks.jamesxu.tutor.Objects
 
-class Offer (val uid: String, val name: String, val major: String, val price: String, val profileImg : String)
+class Offer (val uid: String, val name: String, val major: String, val price: String, val profileImg : String) {
+    constructor() : this("","","","","")
+}
